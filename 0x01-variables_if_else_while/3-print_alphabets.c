1502f362e29113c3abdc1206ab0091f34d2dbf55
Shavz <shavin.l@hotmail.com>
2022-06-03 04:38:37 -0700

    3. alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,16 +8,15 @@
 int main(void)
 {
 	int n;
-	for (n= ‘a’; n<= ‘z’; n++)
+	for (n = 'a'; n <= 'z'; n++)
 	{
 		putchar(n);
 	}
 
-	for (n= ‘A’; n<= ‘Z’; n++)
+	for (n = 'A'; n <= 'Z'; n++)
 	{
 		putchar(n);
-		putchar('\n');
 	}
-
+	putchar('\n');
 	return (0);
 }
