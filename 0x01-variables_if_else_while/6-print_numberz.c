225eb831918e7eff289ee641cc7ba39de35851d6
Shavz <shavin.l@hotmail.com>
2022-06-03 05:24:27 -0700

    6. Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - entry point 
+ * print single digit numbers of base 10
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		putchar("%n", n);
+	}
+	putchar('\n');
+	return (0);
+}
