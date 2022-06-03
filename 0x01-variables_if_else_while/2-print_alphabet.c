a205748ebcc1c298083f0ac5383ab7333ce8b7b3
Shavz <shavin.l@hotmail.com>
2022-06-03 02:55:53 -0700

    2. Print alphabet lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * print alphabet
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n;
+	for (n= ‘a’; n<= ‘z’; n++)
+	{
+		putchar(n);
+	}
+
+	return (0);
+}
