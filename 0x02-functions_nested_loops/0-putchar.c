cc0a1cdca87ebb33f80af79424cf1142b297234a
Shavz <shavin.l@hotmail.com>
2022-06-07 01:21:05 -0700

    0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - entry point
+ * prints _putchar followed by a new line
+ * Return: 0
+ */
+
+int main(void)
+{
+	char x = "_putchar"
+
+		while(x)
+		{
+			putchar(x);
+			x++;
+		}
+	putchar('\n');
+
+	return(0);
+}
