40ef52160c69fdb57b1751fd0a1046685f00de4c
Shavz <shavin.l@hotmail.com>
2022-06-07 10:54:45 -0700

    15. Even Liber Abbaci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * sum even fibonacci numbers under 4 million
+ * Return: 0
+ */
+
+int main(void)
+{
+	long int a, b, c, sum;
+	int n;
+
+	a = 0;
+	b = 1;
+	sum = 0;
+	for (n = 0; n < 50; n++)
+	{
+		c = a + b;
+		a = b;
+		b = c;
+		if (c % 2 == 0 && c < 4000000)
+		{
+			sum = sum + c;
+		}
+	}
+	printf("%ld\n", sum);
+	return (0);
+}	
