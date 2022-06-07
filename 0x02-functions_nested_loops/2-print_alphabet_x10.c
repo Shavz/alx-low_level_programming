b8e741c90b331b2b0b0d3ccbefcf8dd44b10753c
Shavz <shavin.l@hotmail.com>
2022-06-07 04:22:42 -0700

    2. 10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_alphabet_x10 - function that prints the alphabet 10 times 
+ * print_alphabet_x10 - function that prints the alphabet 10 times
  * followed by a new line
  * Return: 0
  */
