34e5b7aa5c037d6cd401b58ef051d804ae1ae793
Shavz <shavin.l@hotmail.com>
2022-06-07 03:09:27 -0700

    10. a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - function that adds two integers and returns the result
+ * @x - first number input
+ * @y - second number input
+ * Return: The result of the addition
+ */
+
+int add(int x, int y)
+{
+	return(x + y);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -8,4 +8,5 @@ int _abs(int);
 int print_last_digit(int nld);
 void jack_bauer(void);
 void times_table(void);
+int add(int x, int y);
 
