8556dd1e41c8ff0733b17d14044df20c9077bd89
Shavz <shavin.l@hotmail.com>
2022-06-13 04:24:17 -0700

    Task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include"main.h"
+/**
+ * puts2 - function
+ * @str: pointer of parameter
+ */
+
+void puts2(char *str)
+{
+	int a = 0;
+
+	while (str[a] != '\0')
+	{
+		if (a % 2 == 0)
+		{
+			_putchar(str[a]);
+		}
+		a++;
+	}
+	_putchar(10);
+}
