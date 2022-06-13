25007cbdb6d0882b6ccfdfd4008eca2e6de7f9d5
Shavz <shavin.l@hotmail.com>
2022-06-13 03:59:53 -0700

    Task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_rev - function
+ * @s: pointer of parameter
+ * Return: 0
+ */
+
+void print_rev(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+	while (len > 0)
+	{
+		_putchar(s[--length]);
+	}
+	_putchar(10);
+}
