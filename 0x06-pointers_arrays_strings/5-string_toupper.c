b8823de74320178b297bee3e42e5c308063b6fbe
Shavz <shavin.l@hotmail.com>
2022-06-15 02:19:41 -0700

    5. Always look up

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * string_toupper - change lowercase to uppercase
+ * @s:string
+ * Return: 0
+ */
+
+char *string_toupper(char *s)
+{
+	int a = 0;
+
+	while (*(s + a))
+	{
+		if ((*(s + a) >= 97) &&  (*(s + a) <= 122))
+		{
+			*(s + a) = *(s + a) - 32;
+		}
+		a++;
+	}
+	return (s);
+}
