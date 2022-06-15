11524198b89d6cd2786c656891fc66f417709783
Shavz <shavin.l@hotmail.com>
2022-06-15 02:28:28 -0700

    6. Capitalize string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * cap_string - capitalizes everey word of a string
+ * s: string 
+ * Return: 0
+ */
+
+char *cap_string(char *s)
+{
+	int x = 0;
+	int y;
+	char a[] = " \t\n,;.!?\"(){}";
+
+	while (*(s + x))
+	{
+		if (*(s + x) >= 'a' && *(s + x) <= 'z')
+		{
+			if (x == 0)
+			{
+				*(s + x) -= 'a' - 'A';
+			}
+			else 
+			{
+				for (y = 0; y <= 12; y++)
+				{
+					if (a[y] == *(s + x - 1))
+					{
+						*(s + x) -= 'a' - 'A';
+					}
+				}
+			}
+		}
+		i++;
+	}
+	return (s);
+}
