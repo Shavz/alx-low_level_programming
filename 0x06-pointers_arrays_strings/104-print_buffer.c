385efc84907675f704ed39ce00d1acd76b927d13
Shavz <shavin.l@hotmail.com>
2022-06-16 14:19:03 -0700

    12. Prints a buffer

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -10,50 +10,33 @@
 
 void print_buffer(char *b, int size)
 {
-	int i, j;
+	int x, i;
 
-	for (i = 0; i < size; i += 10)
+	for (x = 0; x < size; x += 10)
 	{
-		printf("%08x: ", i);
-		for (j = 0; j < 10; j++)
+		printf("%08x: ", x);
+		for (i = 0; i < 10; i++)
 		{
-			if ((j + i) >= size)
-			{
+			if ((i + x) >= size)
 				printf("  ");
-			}
 			else
-			{
-				printf("%02x", *(b + j + i));
-			}
-			if ((j % 2) != 0 && j != 0)
-			{
+				printf("%02x", *(b + i + x));
+			if ((i % 2) != 0 && i != 0)
 				printf(" ");
-			}
 		}
-		for (j = 0; j < 10; j++)
+		for (i = 0; i < 10; i++)
 		{
-			if ((j + i) >= size)
-			{
+			if ((i + x) >= size)
 				break;
-			}
+			else if (*(b + i + x) >= 31 && *(b + i + x) <= 126)
+				printf("%c", *(b + i + x));
 			else
-				if (*(b + j + i) >= 31 && *(b + j + i) <= 126)
-				{
-					printf("%c", *(b + j + i));
-				}
-			else 
-			{
 				printf(".");
-			}
 		}
-		if (i >= size)
-		{
+		if (x >= size)
 			continue;
-		}
 		printf("\n");
 	}
 	if (size <= 0)
-	{
 		printf("\n");
-	}
-}				
+}
