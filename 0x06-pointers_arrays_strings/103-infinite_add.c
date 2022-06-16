49949ad22182584cdc93f22c6c39b27ceb1b3467
Shavz <shavin.l@hotmail.com>
2022-06-16 12:18:02 -0700

    11. Adds two numbers

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -14,12 +14,12 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 
 	for (a = 0; n1[a]; a++)
 	;
-		for (b = 0; n2[b]; b++)
-		;
-			if (a > size_r || b > size_r)
-			{
-				return (0);
-			}
+	for (b = 0; n2[b]; b++)
+	;
+		if (a > size_r || b > size_r)
+		{
+			return (0);
+		}
 	y = 0;
 	for (a -= 1, b -= 1, c = 0; c < size_r - 1; a--, b--, c++)
 	{
