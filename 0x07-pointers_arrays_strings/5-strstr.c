88e865602356a49bdaea2a5d48a50180321f97f2
Shavz <shavin.l@hotmail.com>
2022-06-20 07:39:48 -0700

    5. strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -24,7 +24,7 @@ char *_strstr(char *haystack, char *needle)
 					return (haystack);
 					i++;
 				}
-			} while (haystack[x] == needle[x]);
+			} while (haystack[i] == needle[i]);
 		}
 		haystack++;
 	}
