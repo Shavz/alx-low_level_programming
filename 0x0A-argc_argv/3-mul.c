5535ccb5331bb4439d3767474ee3e7d60f211b3c
Shavz <shavin.l@hotmail.com>
2022-06-23 23:41:52 -0700

    Task 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - multiplication of two arguments
+ * @argc: length of the arguments
+ * @argv: arguments
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	return (0);
+}
