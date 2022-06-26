ca96dcc4ba5b3509d5e699b6675802a133808f13
Shavz <shavin.l@hotmail.com>
2022-06-26 04:58:39 -0700

    Task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,4 +1,6 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 /**
  * create_array - allocate space for an array
  * @size:unssigned int
@@ -14,7 +16,7 @@ char *create_array(unsigned int size, char c)
 	x = malloc(sizeof(char) * size);
 	if (size == 0 || !x)
 	{
-		return (0);
+		return (NULL);
 	}
 	else
 	{
