26099a904ef1bb1c73590bb26388deb162311fca
Shavz <104979137+Shavz@users.noreply.github.com>
2022-06-30 17:32:02 +0200

    Update 101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -2,173 +2,128 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * _calloc - allocate (`size' * `nmemb') bytes and set to 0
- * @nmemb: number of elements
- * @size: number of bytes per element
- *
- * Return: pointer to memory, or NULL if `nmemb' or `size' is 0 or malloc fails
+ * main - multiplies two positive numbers
+ * @argc: argument count
+ * @argv: argument vectors
+ * Return: 0
  */
-void *_calloc(unsigned int nmemb, unsigned int size)
+int main(int argc, char *argv[])
 {
-	unsigned int i;
-	char *p;
+	char *f = argv[1];
+	char *s = argv[2];
 
-	if (size == 0 || nmemb == 0)
-		return (NULL);
-	p = malloc(nmemb * size);
-	if (p == NULL)
-		return (NULL);
-	for (i = 0; i < nmemb * size; ++i)
-		p[i] = 0;
-	return (p);
+	if (argc != 3 || !onlyNumbers(f) || !onlyNumbers(s))
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	if (*f == 48 || *s == 48)
+		printf("0\n");
+	else
+		multiply(s, f);
+	return (0);
 }
 
 /**
- * _strdigit - check if string `s' is composed only of digits
- * @s: string to check
- *
- * Return: 1 if true, 0 if false
+ * multiply - multiplies two numbers and displays it
+ * @f: first "number"
+ * @s: second "number"
  */
-int _strdigit(char *s)
+void multiply(char *f, char *s)
 {
-	if (*s == '-' || *s == '+')
-		++s;
-	while (*s)
+	int i, len1, len2, total, fdigit, sdigit, res = 0, tmp;
+	int *ptr;
+
+	len1 = _strlen(f);
+	len2 = _strlen(s);
+	tmp = len2;
+	total = len1 + len2;
+	ptr = _calloc(sizeof(int), (len1 + len2));
+	for (len1--; len1 >= 0; len1--)
 	{
-		if (*s < '0' || *s > '9')
+		fdigit = f[len1] - '0';
+		res = 0;
+		len2 = tmp;
+		for (len2--; len2 >= 0; len2--)
 		{
-			return (0);
+			sdigit = s[len2] - '0';
+			res += ptr[len2 + len1 + 1] + (fdigit * sdigit);
+			ptr[len1 + len2 + 1] = res % 10;
+			res /= 10;
 		}
-		++s;
+		if (res)
+			ptr[len1 + len2 + 1] = res % 10;
+	}
+	while (*ptr == 0)
+	{
+		ptr++;
+		total--;
+	}
+	for (i = 0; i < total; i++)
+		printf("%i", ptr[i]);
+	printf("\n");
+}
+/**
+ * onlyNumbers - determines if string has only numbers
+ * @c: input string
+ * Return: 0 if false, 1 if true
+ */
+int onlyNumbers(char *c)
+{
+	while (*c)
+	{
+		if (*c < '0' || *c > '9')
+			return (0);
+		c++;
 	}
 	return (1);
 }
 
 /**
- * _puts - print string `s'
- * @s: string to print
- */
-void _puts(char *s)
-{
-	while (*s)
-		_putchar(*(s++));
-}
-
-/**
- * rev_num_str - reverse a number string up to trailing zeros
- * @start: beginning of number
- * @end: end of number
- * @ns: string containing number
- */
-void rev_num_str(int start, int end, char *ns)
-{
-	int i, j;
-	char tmp;
-
-	while (ns[end] == 0 && end != start)
-		--end;
-	for (i = start, j = end; i <= j; ++i, --j)
-	{
-		tmp = ns[i] + '0';
-		ns[i] = ns[j] + '0';
-		ns[j] = tmp;
-	}
-}
-
-/**
- * _strlen - calculate length of string `s'
- * @s: string to get length of
- *
+ * _strlen - returns the length of a string
+ * @s: string s
  * Return: length of string
  */
 int _strlen(char *s)
 {
-	int i;
+	char *p = s;
 
-	for (i = 0; s[i]; ++i)
-		;
-	return (i);
+	while (*s)
+		s++;
+	return (s - p);
 }
 
 /**
- * strmul - multply two numbers as strings
- * @a: first number
- * @b: second number
- *
- * Return: pointer to result on success, or NULL on failure
+ * _memset - fills memory with a constant byte
+ * @s: memory area
+ * @b: constant byte
+ * @n: bytes of the memory area
+ * Return: pointer to the memory area s
  */
-char *strmul(char *a, char *b)
+char *_memset(char *s, char b, unsigned int n)
 {
-	int la, lb, i, j, k, l, neg = 0;
-	char *result;
-	char mul, mul_carry, sum, sum_carry;
+	char *ptr = s;
 
-	if (*a == '-')
-	{
-		neg ^= 1;
-		++a;
-	}
-	if (*b == '-')
-	{
-		neg ^= 1;
-		++b;
-	}
-	la = _strlen(a);
-	lb = _strlen(b);
-	result = _calloc(la + lb + 1 + neg, sizeof(char));
-	if (result == NULL)
+	while (n--)
+		*s++ = b;
+	return (ptr);
+}
+
+/**
+ * _calloc - allocates memory for an array, using malloc
+ * @nmemb: number of elements of pointer
+ * @size: size of each member
+ * Return: pointer of allocated memory
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *ptr;
+
+	if (!nmemb || !size)
 		return (NULL);
-	if (neg)
-		result[0] = '-';
-	for (i = lb - 1, l = neg; i >= 0; --i, ++l)
-	{
-		mul_carry = 0;
-		sum_carry = 0;
-		for (j = la - 1, k = l; j >= 0; --j, ++k)
-		{
-			mul = (a[j] - '0') * (b[i] - '0') + mul_carry;
-			mul_carry = mul / 10;
-			mul %= 10;
-			sum = result[k] + mul + sum_carry;
-			sum_carry = sum / 10;
-			sum %= 10;
-			result[k] = sum;
-		}
-		result[k] = sum_carry + mul_carry;
-	}
-	rev_num_str(neg, k, result);
-	return (result);
-}
-
-/**
- * main - multiply two numbers from the command line and print the result
- * @argc: argument count
- * @argv: argument list
- *
- * Return: 0 if successful, 98 if failure
- */
-int main(int argc, char *argv[])
-{
-	char *result;
-
-	if (argc != 3)
-	{
-		_puts("Error\n");
-		exit(98);
-	}
-	if (!_strdigit(argv[1]) || !_strdigit(argv[2]))
-	{
-		_puts("Error\n");
-		exit(98);
-	}
-	result = strmul(argv[1], argv[2]);
-	if (result == NULL)
-	{
-		_puts("Error\n");
-		exit(98);
-	}
-	_puts(result);
-	_putchar('\n');
-	free(result);
-	exit(EXIT_SUCCESS);
+	ptr = malloc(size * nmemb);
+	if (!ptr)
+		return (NULL);
+	_memset(ptr, 0, size * nmemb);
+	return (ptr);
 }
