0cb62bb84ce463e3b5cf3c2d3b5bf595c2795fe6
Shavz <104979137+Shavz@users.noreply.github.com>
2022-07-06 14:49:03 +0200

    Update 3-get_op_func.c

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -19,11 +19,12 @@ int (*get_op_func(char *s))(int, int)
 	};
 	int i = 0;
 
-	while (i < 5)
+	while (i < 10)
 	{
-		if (strcmp(s, ops[i].op) == 0)
-			return (ops[i].f);
+		if (s[0] == ops->op[i])
+			break;
 		i++;
 	}
-	return (NULL);
+
+	return (ops[i / 2].f);
 }
