315d1510439e1728d32972ce7642eb22daffc0bc
Shavz <104979137+Shavz@users.noreply.github.com>
2022-07-06 14:56:05 +0200

    Update 3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -10,7 +10,7 @@
 
 int main(int argc, char *argv[])
 {
-	int a, b, c;
+	int a, b;
 	int (*operation)(int, int);
 
 	if (argc != 4)
@@ -18,15 +18,23 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(98);
 	}
-	a = atoi(argv[1]);
-	b = atoi(argv[3]);
+if (argv[2][1])
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
 	operation = get_op_func(argv[2]);
+
 	if (operation == NULL)
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	c = operation(a, b);
-	printf("%d\n", c);
+
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+
+	printf("%d\n", operation(a, b));
 	return (0);
 }
