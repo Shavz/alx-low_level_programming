24fd574c72e5e1f14589e9ab523a035d0148a7dc
Shavz <shavin.l@hotmail.com>
2022-07-06 02:22:38 -0700

    Task 3

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,32 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - performs simple operations
+ * @argc: has the length of the arguments
+ * @argv: has the arguments
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int a, b, c;
+	int (*operation)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	operation = get_op_func(argv[2]);
+	if (fun == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	c = operation(a, b);
+	printf("%d\n", c);
+	return (0);
+}
