be3eae863d379220e1b6c5c8e9a8ce193397bdd9
Shavz <104979137+Shavz@users.noreply.github.com>
2022-07-21 14:05:18 +0200

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -12,12 +12,8 @@ unsigned int binary_to_uint(const char *b)
 	if (b != NULL)
 	{
 		for (i = 0; b[i] != '\0'; i++)
-		{
 			if (b[i] != 0 + '0' && b[i] != 1 + '0')
-			{
 				return (0);
-			}
-		}
 		for (j = 0; j < i; j++)
 		{
 			num += (b[(i - 1) - j] - '0') * potwo;
